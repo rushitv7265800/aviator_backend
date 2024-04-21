8cf6372f22c2833ea8d239e7d7f7c6ac0632bb32
rushitv7265800 <107460866+rushitv7265800@users.noreply.github.com>
2024-04-21 11:44:18 +0530

    Update config.js

--- config.js
+++ config.js
@@ -1,5 +1,5 @@
 module.exports = {
-  DATABASE:`mongodb+srv://vaghasiyarushit534:Rushit7265@cluster0.50vnkfy.mongodb.net/game,
+  DATABASE:`mongodb+srv://vaghasiyarushit534:Rushit7265@cluster0.50vnkfy.mongodb.net/game`,
   PORT: 5091,
   loweWinAmount: 30,
 };
